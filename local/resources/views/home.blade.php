2fa2710459a5bf6c9779df5921c99005361f9162
muklas sutra wirawan <muklas541@gmail.com>
2016-10-05 20:39:32 +0700

    Update home.blade.php

--- local/resources/views/home.blade.php
+++ local/resources/views/home.blade.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Bootstrap Example</title>
+  <title>Table list</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
